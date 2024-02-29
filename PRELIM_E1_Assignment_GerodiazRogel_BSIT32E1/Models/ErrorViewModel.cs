922686473c18bb0428b9f91bb4895a2785fad8f6
GerodiazRogel-BSIT32E1 <gerodiazrogel1@gmail.com>
2024-02-29 13:35:15 +0800

    Add project files.

---
+++ PRELIM_E1_Assignment_GerodiazRogel_BSIT32E1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E1_Assignment_GerodiazRogel_BSIT32E1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
